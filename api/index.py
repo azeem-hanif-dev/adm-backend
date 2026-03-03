973e919dfcfa24d48529358e5aea6acc509bad65
Muhammad Azeem <81457043+azeem-hanif-dev@users.noreply.github.com>
2026-03-03 14:02:08 +0500

    test build

--- api/index.py
+++ api/index.py
@@ -553,6 +553,8 @@ async def list_campaigns(
         raise HTTPException(status_code=500, detail=str(e))
 
 
+
+
 # if __name__ == "__main__":
 #     import uvicorn
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
